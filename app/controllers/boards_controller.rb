92a9e98374f439b8cc4b2a99868a73d27b7afb08
numumi <uma_is_shika@hotmail.co.jp>
2024-02-27 00:40:22 +0900

    boards#showにimage追加

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -6,14 +6,34 @@ class BoardsController < SecuredController
       {
         id: board.id,
         name: board.name,
-        image: board.image.attached? ? rails_blob_url(board.image) : nil
+        image: image_url(board.image)
       }
     end
     render json: boards_with_image_urls
   end
 
   def show
-    board_data = Board.includes(columns: :tasks).find(params[:id]).as_json(include: { columns: { only: [:id, :name] } }, only: [:id, :name])
+    board = Board.includes(columns: :tasks).find(params[:id])
+    board_data = board.as_json(
+      include: {
+        columns: {
+          only: [:id, :name],
+          include: {
+            tasks: {
+              only: [:id, :name]
+            }
+          }
+        },
+      },
+      only: [:id, :name],
+      
+    ).merge(image: image_url(board.image))
     render json: board_data
+  end
+
+  private
+
+  def image_url(image)
+    image.attached? ? rails_blob_url(image) : nil
   end
 end
